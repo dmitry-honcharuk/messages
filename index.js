cbdb6d054a2ee0483fee49356b96959a9de438f5
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2021-03-12 20:20:59 +0200

    creates basic broadcasting of incoming messages

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const { Server } = require('socket.io');
 
 const PORT = 9999;
+
+const TOPICS = {
+  LIST_ADDED: 'LIST_ADDED',
+  ITEM_ADDED: 'ITEM_ADDED',
+};
+
+const io = new Server(http, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on(TOPICS.LIST_ADDED, (msg) => {
+    socket.broadcast.emit(TOPICS.LIST_ADDED, msg);
+  });
+
+  socket.on(TOPICS.ITEM_ADDED, (msg) => {
+    socket.broadcast.emit(TOPICS.ITEM_ADDED, msg);
   });
 });
 
